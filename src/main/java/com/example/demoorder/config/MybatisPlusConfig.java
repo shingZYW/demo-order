61230ea407e0553bb411858c080c76582323bbe2
liminglong <edc1247@163.com>
2020-10-30 14:28:42 +0800

    [master][提交test]

--- src/main/java/com/example/demoorder/config/MybatisPlusConfig.java
+++ src/main/java/com/example/demoorder/config/MybatisPlusConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MybatisPlusConfig {
 
+    //dddd
     @Bean
     public PaginationInterceptor paginationInterceptor(){
         PaginationInterceptor page = new PaginationInterceptor();
