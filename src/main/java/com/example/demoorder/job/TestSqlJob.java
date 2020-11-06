1b33cb3de4e2340544942febfc7e17527f7afaae
Zhangyiwen <zhang.yiwen818@iwhalecloud.com>
2020-11-06 13:17:51 +0800

    [demo-job]项目启动就执行一次定时任务

--- src/main/java/com/example/demoorder/job/TestSqlJob.java
+++ src/main/java/com/example/demoorder/job/TestSqlJob.java
@@ -8,13 +8,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
 @Component
-
+@Service
 public class TestSqlJob {
 
     private static Logger logger = LoggerFactory.getLogger(TestSqlJob.class);
@@ -26,11 +28,12 @@ public class TestSqlJob {
     private ShingTestService shingTestService;
 
     /**
-     * 每隔1分钟执行一次
+     * 每隔3分钟执行一次
      */
-
-    @Scheduled(cron = "0 */3 * * * ?")
+    @PostConstruct
+    @Scheduled(cron = "0 0/3 * * * ?")
     public void testSqlJob() {
+
 
         logger.info("=========================定时任务查询开始=======================================");
         List<TableQuery> list1 = testSqlService.testNotExistsAndSubQuery(66L);
